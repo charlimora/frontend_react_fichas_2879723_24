1109ef8292df12ee5b7908f144bdd356108d9824
charlimora <ca.agudelo.2021@gmail.com>
2024-10-24 08:49:47 -0500

    Comenzando proyecto. Componente Anuncio y sus estilos

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './css/Announcement.module.css'
+
+const Announcement = () => {
+  return (
+    <div className={styles.container}>
+        Grandes descuentos para ti, todos los días
+    </div>
+  )
+}
+
+export default Announcement
